39567957ebeecfb8d9fc3d7c08e3f1625ca8ddc3
tancheff <k_tanchev@yahoo.co.uk>
2025-01-22 19:27:03 +0200

    adding new solutions

--- 03_Migrations_and_Django_Admin/caller.py
+++ 03_Migrations_and_Django_Admin/caller.py
@@ -8,3 +8,7 @@ django.setup()
 # Import your models here
 
 # Create queries within functions
+
+from main_app.models import Shoe
+
+print(Shoe.objects.values_list("brand", flat=True).distinct()) # select brand from main_app_shoe
--- 03_Migrations_and_Django_Admin/main_app/models.py
+++ 03_Migrations_and_Django_Admin/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Shoe(models.Model):
+    brand = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+
+
+class UniqueBrands(models.Model):
+    brand = models.CharField(max_length=25, unique=True)
+
+# създаваме таблица с уникални имена:
+# makemigrations main_app --name migrate_unique_brands --empty
