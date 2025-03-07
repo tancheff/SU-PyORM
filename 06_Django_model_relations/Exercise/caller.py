620d62302ff32cb564c42c21aca214357395b461
tancheff <k_tanchev@yahoo.co.uk>
2025-03-07 18:02:38 +0200

    adding solutions

--- 06_Django_model_relations/Exercise/caller.py
+++ 06_Django_model_relations/Exercise/caller.py
@@ -1,4 +1,6 @@
 import os
+from datetime import date, datetime, timedelta
+
 import django
 from django.db.models import QuerySet
 
@@ -7,8 +9,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Author, Book, Artist, Song, Product, Review
-
+from main_app.models import Author, Book, Artist, Song, Product, Review, Driver, DrivingLicense, Owner, Car, Registration
 
 # Create queries within functions
 
@@ -128,12 +129,35 @@ def remove_song_from_artist(artist_name: str, song_title: str) -> None:
 # ------------- 3. Shop -------------
 def calculate_average_rating_for_product_by_name(product_name: str) -> str:
     product = Product.objects.get(name=product_name)
-    all_reviews = product.review_set.all()
+    all_reviews = product.reviews.all()
 
-    avg_rating = sum([r.rating for r in all_reviews])/len([r.rating for r in all_reviews])
+    avg_rating = sum([r.rating for r in all_reviews])/len(all_reviews)
 
-    return f'{avg_rating:.1f}'
+    # return f'{avg_rating:.1f}'
+    return avg_rating
 
+# --- this is a test function
+# def find_products_from_average_rating(rating: float) -> QuerySet[Product]:
+#     reviews = Review.objects.filter(rating=rating)
+#     products = Product.objects.filter(reviews__in=reviews)
+#
+#     return products
+
+# print(find_products_from_average_rating(3.5))
+# ---
+
+def get_reviews_with_high_ratings(threshold: int)-> QuerySet[Review]:
+   reviews = Review.objects.filter(rating__gte=threshold)
+
+   return reviews
+
+def get_products_with_no_reviews() -> QuerySet[Product]:
+    products = Product.objects.filter(reviews__isnull=True).order_by('-name')
+
+    return products
+
+def delete_products_without_reviews() -> None:
+    Product.objects.filter(reviews__isnull=True).delete()
 
 # # Create some products
 # product1 = Product.objects.create(name="Laptop")
@@ -146,16 +170,86 @@ def calculate_average_rating_for_product_by_name(product_name: str) -> str:
 # review2 = Review.objects.create(description="The laptop is slow!", rating=2, product=product1)
 # review3 = Review.objects.create(description="Awesome smartphone!", rating=5, product=product2)
 
-# Calculate and print the average rating
-print(calculate_average_rating_for_product_by_name("Laptop"))
+# # Run the function to get products without reviews
+# products_without_reviews = get_products_with_no_reviews()
+# print(f"Products without reviews: {', '.join([p.name for p in products_without_reviews])}")
+# # Run the function to delete products without reviews
+# delete_products_without_reviews()
+# print(f"Products left: {Product.objects.count()}")
+# # Get reviews with high ratings
+# print(get_reviews_with_high_ratings(3))
+# # Calculate and print the average rating
+# print(calculate_average_rating_for_product_by_name("Laptop"))
+
+# ------------- 4. License -------------
+def calculate_licenses_expiration_dates() -> str:
+    licenses = DrivingLicense.objects.all().order_by('-license_number')
+
+    return "\n".join(str(l) for l in licenses)
+
+def get_drivers_with_expired_licenses(due_date: datetime.date) -> QuerySet[DrivingLicense]:
+    latest_issue_date = due_date - timedelta(days=365)
+
+    # вместо да се прави с 2 заявки, може да се направи само с 1
+    # licenses = DrivingLicense.objects.filter(issue_date__gt=latest_issue_date)
+    # drivers = Driver.objects.filter(license__in=licenses)
+
+    drivers = Driver.objects.filter(license__issue_date__gt=latest_issue_date)
+
+    return drivers
 
 
 
+# # Create drivers
+# driver1 = Driver.objects.create(first_name="Tanya", last_name="Petrova")
+# driver2 = Driver.objects.create(first_name="Ivan", last_name="Yordanov")
+#
+# # Create licenses associated with drivers
+# license1 = DrivingLicense.objects.create(license_number="123", issue_date=date(2022, 10, 6), driver=driver1)
+#
+# license2 = DrivingLicense.objects.create(license_number="456", issue_date=date(2022, 1, 1), driver=driver2)
+
+# Calculate licenses expiration dates
+# expiration_dates = calculate_licenses_expiration_dates()
+# print(expiration_dates)
+
+# # Get drivers with expired licenses
+# drivers_with_expired_licenses = get_drivers_with_expired_licenses(date(2023, 1, 1))
+#
+# for driver in drivers_with_expired_licenses:
+#     print(f"{driver.first_name} {driver.last_name} has to renew their driving license!")
+
+# ------------- 5. Car Registration -------------
+
+def register_car_by_owner(owner: Owner) -> str:
+    registration = Registration.objects.filter(car__isnull=True).first()
+    car = Car.objects.filter(registration__isnull=True).first()
+
+    car.owner = owner
+    car.registration = registration
+    car.save()
+
+    registration.registration_date = datetime.today()
+    registration.car = car
+    registration.save()
 
 
+    return (f"Successfully registered {car.model} to {owner.name} with "
+            f"registration number {registration.registration_number}.")
 
 
+# # Get owners
+# owner1 = Owner.objects.get(name='Ivelin Milchev')
+# owner2 = Owner.objects.get(name='Alice Smith')
+#
+# # Get cars
+# car1 = Car.objects.get(model='Citroen C5', year=2004)
+# car2 = Car.objects.get(model='Honda Civic', year=2021)
+# # Get instances of the Registration model for the cars
+# registration1 = Registration.objects.get(registration_number='TX0044XA')
+# registration2 = Registration.objects.get(registration_number='XYZ789')
 
+# print(register_car_by_owner(owner1))
 
 
 
--- 06_Django_model_relations/Exercise/main_app/models.py
+++ 06_Django_model_relations/Exercise/main_app/models.py
@@ -1,6 +1,10 @@
+from datetime import timedelta
+
 from django.db import models
 
 # Create your models here.
+
+# ------------- 1. Library -------------
 class Author(models.Model):
     name = models.CharField(max_length=40)
 
@@ -12,6 +16,7 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title}"
 
+# ------------- 2. Music App -------------
 class Song(models.Model):
     title = models.CharField(max_length=100, unique=True)
 
@@ -23,6 +28,7 @@ class Artist(models.Model):
     songs = models.ManyToManyField(to=Song, related_name='artists')
     # вместо да пишем .artist_set, достъпваме всички артисти чрез .artists
 
+# ------------- 3. Shop -------------
 class Product(models.Model):
     name = models.CharField(max_length=100)
 
@@ -31,24 +37,30 @@ class Review(models.Model):
     rating = models.PositiveSmallIntegerField()
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE, related_name='reviews')
 
+# ------------- 4. License -------------
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
 
+class DrivingLicense(models.Model):
+    license_number = models.CharField(max_length=10, unique=True)
+    issue_date = models.DateField()
+    driver = models.OneToOneField(to=Driver, on_delete=models.CASCADE, related_name='license') # driver_id is UNIQUE
 
+    def __str__(self):
+        expiration_date = self.issue_date + timedelta(days=365)
+        return f"License with number: {self.license_number} expires on {expiration_date}!"
 
+# ------------- 5. Car Registration -------------
+class Owner(models.Model):
+    name = models.CharField(max_length=50)
 
+class Car(models.Model):
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    owner = models.ForeignKey(to=Owner, on_delete=models.CASCADE, related_name='cars', blank=True, null=True)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Registration(models.Model):
+    registration_number = models.CharField(max_length=10, unique=True)
+    registration_date = models.DateField(blank=True, null=True)
+    car = models.OneToOneField(to=Car, on_delete=models.CASCADE, related_name='registration', blank=True, null=True)
