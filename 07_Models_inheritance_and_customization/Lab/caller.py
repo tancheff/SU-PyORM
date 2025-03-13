e45a5c65df86ff59baa4f2667cd48154671d48a9
tancheff <k_tanchev@yahoo.co.uk>
2025-03-13 12:26:19 +0200

    added solutions

--- 07_Models_inheritance_and_customization/Lab/caller.py
+++ 07_Models_inheritance_and_customization/Lab/caller.py
@@ -8,14 +8,41 @@ django.setup()
 # Import your models here
 
 # Create queries within functions
+from main_app.models import Animal, Mammal, Bird, Reptile, ZooKeeper, Veterinarian
 
-from main_app.models import Animal, Mammal, Bird, Reptile
+# ------------- 1. Zoo Animals -------------
+# Animal.objects.create(name="Nemo", species="Clownfish", birth_date="2019-04-10", sound="Bubbles")
+# Mammal.objects.create(name="Fluffy", species="Orangutan", birth_date="2018-02-10", sound="Chomps", fur_color="Reddish-brown")
+# Bird.objects.create(name="Robby", species="American Robin", birth_date="2021-03-20", sound="Chirp", wing_span=28.50)
+# Reptile.objects.create(name="Python", species="Ball Python", birth_date="2019-07-01", sound="Hiss", scale_type="Smooth")
+#
+# animals = Animal.objects.all()
+# for a in animals:
+#     print(f"{a.name}: {a.species}.")
 
-Animal.objects.create(name="Nemo", species="Clownfish", birth_date="2019-04-10", sound="Bubbles")
-Mammal.objects.create(name="Fluffy", species="Orangutan", birth_date="2018-02-10", sound="Chomps", fur_color="Reddish-brown")
-Bird.objects.create(name="Robby", species="American Robin", birth_date="2021-03-20", sound="Chirp", wing_span=28.50)
-Reptile.objects.create(name="Python", species="Ball Python", birth_date="2019-07-01", sound="Hiss", scale_type="Smooth")
 
-animals = Animal.objects.all()
-for a in animals:
-    print(f"{a.name}: {a.species}.")
+# ------------- 2. Zoo Employees -------------
+# zookeeper = ZooKeeper.objects.create(first_name="Peter", last_name="Johnson", phone_number="0899524265", specialty="Mammals")
+# mammal = Mammal.objects.get(name="Fluffy")
+# zookeeper.managed_animals.add(mammal)
+# veterinarian = Veterinarian.objects.create(first_name="Dr. Michael", last_name="Smith", phone_number="9876543210", license_number="VET12345")
+#
+# zookeeper_from_db = ZooKeeper.objects.first()
+# print(f"{zookeeper_from_db.first_name} {zookeeper_from_db.last_name} is a ZooKeeper.")
+#
+# veterinarian_from_db = Veterinarian.objects.first()
+# print(f"{veterinarian_from_db.first_name} {veterinarian_from_db.last_name} is a Veterinarian.")
+
+# print(ZooKeeper.Specialities.Birds)
+
+# ------------- 3. Animal Display System -------------
+# from main_app.models import ZooDisplayAnimal
+#
+# is_proxy = ZooDisplayAnimal._meta.proxy
+#
+# if is_proxy:
+#     print("ZooDisplayAnimal is a proxy model.")
+# else:
+#     print("ZooDisplayAnimal is not a proxy model.")
+
+
--- 07_Models_inheritance_and_customization/Lab/main_app/models.py
+++ 07_Models_inheritance_and_customization/Lab/main_app/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+# # примерни класове, които да се наслеяват от всички таблици
+
+# class TimeStampModel(models.Model):
+#     created_at = models.DateTimeField()
+#     updated_at = models.DateTimeField()
+#
+#     class Meta:
+#         abstract = True
+
+# class SoftDeleteModel(models.Model):
+#     is_deleted = models.BooleanField(default=False)
+#
+#     class Meta:
+#         abstract = True
+
+# ------------- 1. Zoo Animals -------------
 class Animal(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -16,3 +32,40 @@ class Bird(Animal):
 
 class Reptile(Animal):
     scale_type = models.CharField(max_length=50)
+
+# ------------- 2. Zoo Employees -------------
+class EmployeeBaseClass(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+SPECIALITIES = (
+    ("Mammals", "Mammals"),
+    ("Birds", "Birds"),
+    ("Reptiles", "Reptiles"),
+    ("Others", "Others")
+)
+
+class ZooKeeper(EmployeeBaseClass):
+    class Specialities(models.TextChoices):
+        Mammals = "Mammals"
+        Birds = "Birds"
+        Reptiles = "Reptiles"
+        Others = "Others"
+
+    specialty = models.CharField(max_length=10, choices=Specialities.choices)
+    managed_animals = models.ManyToManyField(to='Animal')
+
+    def clean(self):
+        super().clean()
+
+class Veterinarian(EmployeeBaseClass):
+    license_number = models.CharField(max_length=10)
+
+# ------------- 3. Animal Display System -------------
+class ZooDisplayAnimal(Animal):
+    class Meta:
+        proxy = True
