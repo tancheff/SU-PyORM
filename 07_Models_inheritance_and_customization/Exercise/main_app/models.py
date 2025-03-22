99047efd560f96d49e589a76b2abe5a70d641e93
tancheff <k_tanchev@yahoo.co.uk>
2025-03-22 23:53:13 +0200

    adding solutions

--- 07_Models_inheritance_and_customization/Exercise/main_app/models.py
+++ 07_Models_inheritance_and_customization/Exercise/main_app/models.py
@@ -1,7 +1,12 @@
+from datetime import timedelta
+from decimal import Decimal
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
 
+# ------------- 1. Character Classes -------------
 class BaseCharacter(models.Model):
     class Meta:
         abstract = True
@@ -41,3 +46,198 @@ class VengeanceDemonHunter(DemonHunter):
 
 class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(max_length=100)
+
+# ------------- 2. Chat App -------------
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+class Message(models.Model):
+    sender = models.ForeignKey(to=UserProfile, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(to=UserProfile, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self) -> None:
+        self.is_read = True
+
+    def reply_to_message(self, reply_content: str) -> 'Message':
+        message = Message.objects.create(sender=self.receiver, receiver=self.sender, content=reply_content)
+
+        return message
+
+    def forward_message(self, receiver: UserProfile) -> 'Message':
+        message = Message.objects.create(sender=self.receiver, receiver=receiver, content=self.content)
+
+        return message
+
+# ------------- 3. Student Information -------------
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value) -> int or None:
+        try:
+            return int(value)  # int("dido")
+        except ValueError:
+            raise ValueError("Invalid input for student ID")
+
+    def get_prep_value(self, value):
+        cleaned_value = self.to_python(value)
+
+        if cleaned_value <= 0:
+            raise ValidationError("ID cannot be less than or equal to zero")
+
+        return cleaned_value
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = StudentIDField()
+
+# ------------- 4. Credit Card Masking -------------
+class MaskedCreditCardField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 20
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value) -> str:
+        if not isinstance(value, str):
+            raise ValidationError("The card number must be a string")
+
+        if not value.isdigit():
+            raise ValidationError("The card number must contain only digits")
+
+        if len(value) != 16:
+            raise ValidationError("The card number must be exactly 16 characters long")
+
+        return f"****-****-****-{value[-4:]}"
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(max_length=100)
+    card_number = MaskedCreditCardField()
+
+# ------------- 5. *Hotel Reservation System -------------
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+class Room(models.Model):
+    hotel = models.ForeignKey(to=Hotel, on_delete=models.CASCADE)
+    number = models.CharField(max_length=100, unique=True)
+    capacity = models.PositiveIntegerField()
+    total_guests = models.PositiveIntegerField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def clean(self) -> None:    # used for model validation
+        if self.total_guests > self.capacity:
+            raise ValidationError("Total guests are more than the capacity of the room")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+        return f"Room {self.number} created successfully"
+
+class BaseReservation(models.Model):
+    class Meta:
+        abstract = True
+
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    # usually a defined as a property
+    def reservation_period(self) -> int:
+        duration_of_stay = (self.end_date - self.start_date).days
+        return duration_of_stay
+
+    # usually a defined as a property
+    def calculate_total_cost(self) -> Decimal:
+        cost = self.reservation_period() * self.room.price_per_night
+
+        return round(cost, 2)
+
+    @property
+    def is_available(self) -> bool:
+        reservations = self.__class__.objects.filter(       # това филтрира класа, който наследява BaseReservation
+            room=self.room,
+            end_date__gte=self.start_date,
+            start_date__lte=self.end_date
+        )
+        return not reservations.exists()
+
+    def clean(self) -> None:
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+
+        if not self.is_available:
+            raise ValidationError(f"Room {self.room.number} cannot be reserved")
+
+
+class RegularReservation(BaseReservation):
+    def save(self, *args, **kwargs) -> str:
+        super().clean()
+
+        super().save(*args, **kwargs)
+
+        return f"Regular reservation for room {self.room.number}"
+
+
+class SpecialReservation(BaseReservation):
+    def extend_reservation(self, days: int) -> str:
+        self.end_date += timedelta(days=days)
+
+        if not self.is_available:
+            raise ValidationError("Error during extending reservation")
+
+        self.save()
+
+        return f"Extended reservation for room {self.room.number} with {days} days"
+
+
+
+    # could avoid repetition with @property
+    def save(self, *args, **kwargs) -> str:
+        super().clean()
+
+        super().save(*args, **kwargs)
+
+        return f"Regular reservation for room {self.room.number}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
