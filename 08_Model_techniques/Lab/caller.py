8441e60f59da2881ba9b85ae434d7ecdd4163eb0
tancheff <k_tanchev@yahoo.co.uk>
2025-03-23 18:41:43 +0200

    adding solutions

--- 08_Model_techniques/Lab/caller.py
+++ 08_Model_techniques/Lab/caller.py
@@ -8,7 +8,7 @@ django.setup()
 # Import your models here
 
 # Create queries within functions
-from main_app.models import Restaurant, Menu, RestaurantReview, RegularRestaurantReview, FoodCriticRestaurantReview
+from main_app.models import Restaurant, Menu, RestaurantReview, RegularRestaurantReview, FoodCriticRestaurantReview, MenuReview
 from django.core.exceptions import ValidationError
 #
 # valid_restaurant = Restaurant(
@@ -108,5 +108,26 @@ from django.core.exceptions import ValidationError
 # print(f"Model Name: {FoodCriticRestaurantReview._meta.verbose_name}")
 # print(f"Model Plural Name: {FoodCriticRestaurantReview._meta.verbose_name_plural}")
 
-# ------------- 5. Menu Review -------------
+# ------------- 06. Rating and Review Content -------------
+Restaurant.objects.create(name="Savory Delight", location="456 Elm Avenue", rating=4.2,)
+restaurant_from_db = Restaurant.objects.get(name="Savory Delight")
+RegularRestaurantReview.objects.create(reviewer_name="Alice", restaurant=restaurant_from_db, rating=4, review_content="Good experience overall.")
+review_from_db = RegularRestaurantReview.objects.get(reviewer_name="Alice", restaurant=restaurant_from_db)
+print(
+    f"Reviewer name: {review_from_db.reviewer_name}\n"
+    f"Restaurant: {review_from_db.restaurant.name}\n"
+    f"Rating: {review_from_db.rating}\n"
+    f"Review content: {review_from_db.review_content}"
+)
+
+Menu.objects.create(name="Delightful Food Menu", description="Appetizers:\nSpinach and Artichoke Dip\nMain Course:\nGrilled Salmon\nDesserts:\nChocolate Fondue", restaurant=restaurant_from_db)
+menu_from_db = Menu.objects.get(name="Delightful Food Menu")
+MenuReview.objects.create(reviewer_name="Lilly", menu=menu_from_db, rating=5, review_content="Delicious food")
+menu_review_from_db = MenuReview.objects.get(reviewer_name="Lilly", menu=menu_from_db)
+print(
+    f"Reviewer name: {menu_review_from_db.reviewer_name}\n"
+    f"Menu: {menu_review_from_db.menu.name}\n"
+    f"Rating: {menu_review_from_db.rating}\n"
+    f"Review content: {menu_review_from_db.review_content}"
+)
 
--- 08_Model_techniques/Lab/main_app/models.py
+++ 08_Model_techniques/Lab/main_app/models.py
@@ -6,6 +6,8 @@ from main_app.validators import validate_menu_categories
 
 # Create your models here.
 
+
+
 # ------------- 1. Restaurant -------------
 
 class Restaurant(models.Model):
@@ -43,10 +45,9 @@ class Menu(models.Model):
     )
     restaurant = models.ForeignKey(to=Restaurant, on_delete=models.CASCADE)
 
-# ------------- 3. Restaurant Review -------------
-class RestaurantReview(models.Model):
+# ------------- 06. Rating and Review Content -------------
+class ReviewMixin(models.Model):
     reviewer_name = models.CharField(max_length=100)
-    restaurant = models.ForeignKey(to=Restaurant, on_delete=models.CASCADE)
     review_content = models.TextField()
     rating = models.PositiveIntegerField(
         validators=[
@@ -56,60 +57,56 @@ class RestaurantReview(models.Model):
 
     class Meta:
         ordering = ['-rating']
+        abstract = True
+
+# ------------- 3. Restaurant Review -------------
+class RestaurantReview(models.Model):
+    restaurant = models.ForeignKey(to=Restaurant, on_delete=models.CASCADE)
+    # reviewer_name = models.CharField(max_length=100)
+    # review_content = models.TextField()
+    # rating = models.PositiveIntegerField(
+    #     validators=[
+    #         MaxValueValidator(5)
+    #     ]
+    # )
+
+    class Meta:
+        # ordering = ['-rating']
         verbose_name = 'Restaurant Review'
         verbose_name_plural = 'Restaurant Reviews'
         unique_together = ['reviewer_name', 'restaurant']
         abstract = True
 
 # ------------- 4. Restaurant Review Types -------------
-class RegularRestaurantReview(RestaurantReview):
+class RegularRestaurantReview(ReviewMixin, RestaurantReview):
     pass
 
-class FoodCriticRestaurantReview(RestaurantReview):
+    class Meta(ReviewMixin.Meta, RestaurantReview.Meta):
+        pass
+
+class FoodCriticRestaurantReview(ReviewMixin, RestaurantReview):
     food_critic_cuisine_area = models.CharField(max_length=100)
 
-    class Meta(RestaurantReview.Meta):
+    class Meta(ReviewMixin.Meta, RestaurantReview.Meta):
         verbose_name = 'Food Critic Review'
         verbose_name_plural = 'Food Critic Reviews'
 
 # ------------- 5. Menu Review -------------
-class MenuReview(RestaurantReview):
-    restaurant = None
-    reviewer_name = models.CharField(max_length=100)
+class MenuReview(ReviewMixin):
     menu = models.ForeignKey(to=Menu, on_delete=models.CASCADE)
-    review_content = models.TextField()
-    rating = models.PositiveIntegerField(
-        validators=[
-            MaxValueValidator(5)
-        ]
-    )
+    # reviewer_name = models.CharField(max_length=100)
+    # review_content = models.TextField()
+    # rating = models.PositiveIntegerField(
+    #     validators=[
+    #         MaxValueValidator(5)
+    #     ]
+    # )
 
-    class Meta(RestaurantReview.Meta):
+    class Meta(ReviewMixin.Meta):
+        # ordering = ['-rating']
         verbose_name = 'Menu Review'
         verbose_name_plural = 'Menu Reviews'
         unique_together = ['reviewer_name', 'menu']
         indexes = [
             Index(fields=['menu',], name='main_app_menu_review_menu_id')
         ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
